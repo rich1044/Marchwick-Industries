53e09b30f5e324ff297d8a904f1ecd775b833897
Patrick Richard <Patrick Richard>
2016-10-10 10:55:03 -0500

    initial push

---
+++ scripts/contact.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 44.0856, lng: -93.2259};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
